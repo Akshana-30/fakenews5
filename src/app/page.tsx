aad8a84f80b785b0235da21fba7cdbb4aac2dfa8
Adam Lundvall <adam.lundvall@gmail.com>
2026-05-28 08:36:23 +0200

    Added main container and footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-
-
 export default function HomePage() {
-    return (<div>
-       
-    </div>);
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis repudiandae, suscipit
+            quam laborum error, vel, saepe natus quis vero distinctio similique consectetur
+            voluptatem omnis tenetur maxime earum unde fugit aperiam minima corporis praesentium
+            eligendi tempora delectus quisquam. Deleniti itaque animi obcaecati quas nobis veniam
+            dignissimos eligendi architecto alias odit commodi, amet ipsum atque autem doloremque
+            quia dolores quisquam consequuntur porro beatae. Aspernatur error veniam ducimus? Modi
+            illum, temporibus libero, dolor ab explicabo sequi perferendis adipisci facere,
+            praesentium facilis numquam ipsa dolorem incidunt rerum quos totam neque sapiente autem
+            nisi. Quas officiis quis doloremque non enim corporis ut est asperiores minima!
+        </div>
+    );
 }
