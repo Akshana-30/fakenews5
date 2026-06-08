0766dc08dff5174bd115e7291462aacfacf1b040
Adam Lundvall <adam.lundvall@gmail.com>
2026-06-08 11:10:40 +0200

    installing stripe

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,18 +1,20 @@
 import { createAuthClient } from "better-auth/react";
-import { adminClient } from "better-auth/client/plugins"
+import { adminClient } from "better-auth/client/plugins";
 import { ac, admin, editor, subscriber } from "./permissions";
+import { stripeClient } from "@better-auth/stripe/client";
 
-export const authClient = createAuthClient(
-    {
-        plugins: [
-            adminClient({
-                ac,
-                roles: {
-                    admin,
-                    subscriber,
-                    editor
-                }
-            })
-        ]
-    }
-);
+export const authClient = createAuthClient({
+    plugins: [
+        adminClient({
+            ac,
+            roles: {
+                admin,
+                subscriber,
+                editor,
+            },
+        }),
+        stripeClient({
+            subscription: true, //if you want to enable subscription management
+        }),
+    ],
+});
