24388681ec4f8e889699efcb5643fa8663f84343
Tose2514 <tobias.sederstrom@gmail.com>
2026-07-31 11:00:39 +0200

    category links

--- src/app/article/preview/[articleId]/page.tsx
+++ src/app/article/preview/[articleId]/page.tsx
@@ -37,15 +37,15 @@ export default async function PreviewArticlePage({
                     <div className="flex items-baseline flex-wrap gap-2 mt-2">
                         {parentCategory.map((c, i) => (
                             <div key={c.id}>
-                                <Badge className="bg-primary/60 p-3 text-white text-md hover:bg-primary hover:shadow-gray-500 hover:shadow-2xl dark:hover:shadow-white dark:hover:shadow-2xl dark:hover:bg-primary transition-colors">
+                                 <div className="cursor-pointer border rounded-md px-2 hover:border-b-primary! hover:bg-[#f4ede0]! hover:dark:text-background rounded-b-none text-sm md:text-[16px]">
                                     <Link
-                                        href={`category/${c.id}`}
+                                        href={`/category/${c.id}`}
                                         className="flex items-center gap-1"
                                     >
                                         {c.name}
-                                        <ArrowRight />
+                                        {/* <ArrowRight /> */}
                                     </Link>
-                                </Badge>
+                                </div>
                             </div>
                         ))}
                     </div>
